953f6c50accab616b070f302e81e1301477e5e3a
Gibexz <andrewetedu@gmail.com>
2023-12-16 17:17:06 +0100

    Database intergrations and relationships 2%

--- AppAdmins/models.py
+++ AppAdmins/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class AppAdmin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
     username = models.CharField(max_length=100, unique=True, null=False)
     email =models.CharField(max_length=100, unique=True, null=False)
     password = models.CharField(max_length=100, null=False)
