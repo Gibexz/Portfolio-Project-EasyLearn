2558aa2a8b857bc84b888e3c8d67cb7c28e3dbc3
Gibexz <andrewetedu@gmail.com>
2023-12-16 12:46:47 +0100

    Database intergrations and relationships 1%

--- client/models.py
+++ client/models.py
@@ -1,58 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
-class Clients(models.Model):
-    "Clients database"
+class Client(models.Model):
     username = models.CharField(max_length=100, null=True)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
-    firstName = models.CharField(max_length=100, null=True)
-    lastName = models.CharField(max_length=100, null=True)
-    phoneNumber = models.CharField(max_length=15, null=True, unique=True)
-    stateOfResidence = models.CharField(max_length=50, null=True)
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=15, null=True, unique=True)
+    state_of_residence = models.CharField(max_length=50, null=True)
     nationality = models.CharField(max_length=50, null=True)
-    profilePicture = models.CharField(max_length=150, unique=True)
-    residentialAddress = models.CharField(null=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(default=timezone.now, null=True)
-
+    profile_picture = models.CharField(max_length=150, unique=True)
+    residential_address = models.CharField(max_length=150, null=True)  # Added missing max_length
+    created_at = models.DateTimeField(auto_now_add=True)  # Changed to DateTimeField
+    updated_at = models.DateTimeField(default=timezone.now, null=True)
 
 class Review(models.Model):
-    "Tutor reviews by clients"
-    reviewText = models.CharField(max_length=400)
-    tutorID = models.IntegerField()
-
+    review_text = models.CharField(max_length=400)
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE)  # Assuming Tutor is a User model
 
 class Ranking(models.Model):
-    "Tutor Rankings by clients"
-    rankingText = models.IntegerField()
-    tutorID = models.IntegerField()
-
+    ranking_text = models.IntegerField()
+    tutor = models.ForeignKey(User, on_delete=models.CASCADE)  # Assuming Tutor is a User model
 
 class ClientReportAbuse(models.Model):
-    "Client report abuse models"
-    targetTutorID = models.IntegerField()
+    target_tutor = models.IntegerField()  # Change to ForeignKey if Tutor is a model
     message = models.CharField(max_length=400)
     subject = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)  # Changed to DateTimeField
 
 class Cart(models.Model):
-    "Client report abuse models"
-    targetTutorID = models.IntegerField()
-    tutorsCount = models.IntegerField(null=True)
-    tutorStatus = models.BooleanField()
-
+    target_tutor = models.IntegerField()  # Change to ForeignKey if Tutor is a model
+    tutors_count = models.IntegerField(null=True)
+    tutor_status = models.BooleanField()
 
 class Payment(models.Model):
-    "Client Payment models"
-    tnxID = models.CharField(null=True, max_length=200)
-    time = models.DateField(timezone.now)
+    tnx_id = models.CharField(null=True, max_length=200)
+    time = models.DateTimeField(default=timezone.now)  # Changed to DateTimeField
 
-
-class Payment(models.Model):
-    "Client Payment reciept models"
-    receiptID = models.CharField(null=True, max_length=200)
-    paymentTime = models.DateField(timezone.now)
+class PaymentReceipt(models.Model):  # Changed model name to PaymentReceipt
+    receipt_id = models.CharField(null=True, max_length=200)
+    payment_time = models.DateTimeField(default=timezone.now)  # Changed to DateTimeField
--- tutor/models.py
+++ tutor/models.py
@@ -1,58 +1,57 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.utils import timezone
 
-# Create your models here.
-class Tutors(models.Model):
-    "Tutors database"
-    firstName = models.CharField(max_length=100, null=True)
-    lastName = models.CharField(max_length=100, null=True)
-    email = models.CharField(max_length=100, unique=True)
+class Tutor(models.Model):
+    """Tutors database"""
+    first_name = models.CharField(max_length=100, null=True)
+    last_name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
-    phoneNumber = models.CharField(max_length=15, null=True, unique=True)
+    phone_number = models.CharField(max_length=15, null=True, unique=True)
     gender = models.CharField(max_length=50, null=True)
-    dateOfBirth = models.DateField(default=timezone.now())
-    highestQualification = models.CharField(max_length=50, null=True)
-    employementStatus = models.CharField(max_length=50, null=True)
-    stateOfResidence = models.CharField(max_length=50, null=True)
+    date_of_birth = models.DateField(default=timezone.now)
+    highest_qualification = models.CharField(max_length=50, null=True)
+    employment_status = models.CharField(max_length=50, null=True)
+    state_of_residence = models.CharField(max_length=50, null=True)
     nationality = models.CharField(max_length=50, null=True)
-    areaOfSpacialization = models.CharField(max_length=100, null=True)
-    personalStatement = models.TextField(max_length=5000, null=True)
-    avalability = models.CharField(max_length=50, null=True)
-    workingHours = models.CharField(max_length=500, null=True)
+    area_of_specialization = models.CharField(max_length=100, null=True)
+    personal_statement = models.TextField(max_length=5000, null=True)
+    availability = models.CharField(max_length=50, null=True)
+    working_hours = models.CharField(max_length=500, null=True)
     status = models.BooleanField(null=True)
-    cvID = models.IntegerField(null=True, unique=True)
-    profilePicture = models.CharField(max_length=150, unique=True)
-    residentialAddress = models.CharField(null=True)
-    activeClients = models.IntegerField(default=0, null=True)
-    totalClients = models.IntegerField(default=0, null=True)
-    rejectedClients = models.IntegerField(default=0, null=True)
-    reviewsID = models.IntegerField(null=True, unique=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(default=timezone.now, null=True)
+    cv_id = models.IntegerField(null=True, unique=True)
+    profile_picture = models.CharField(max_length=150, unique=True)
+    residential_address = models.CharField(max_length=255, null=True)
+    active_clients = models.IntegerField(default=0, null=True)
+    total_clients = models.IntegerField(default=0, null=True)
+    rejected_clients = models.IntegerField(default=0, null=True)
+    reviews_id = models.IntegerField(null=True, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now, null=True)
 
 
-class Subjects(models.Model):
-    "Tutors Subject Model"
-    subjectName = models.CharField(max_length=100)
-    nunmberOfTutors = models.IntegerField(null=True)
-    relatedSubjects = models.TextField(max, null=True)
-    created_at= models.DateField(auto_now_add=True)
-    updated_at = models.DateField(default=timezone.now(), null=True)
+class Subject(models.Model):
+    """Tutors Subject Model"""
+    subject_name = models.CharField(max_length=100)
+    number_of_tutors = models.IntegerField(null=True)
+    related_subjects = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now, null=True)
 
 
-class TutorsReportAbuse(models.Model):
-    "Tutors report abuse models"
-    targetClientID = models.IntegerField()
+class TutorReportAbuse(models.Model):
+    """Tutors report abuse models"""
+    target_client_id = models.IntegerField()
     message = models.CharField(max_length=400)
     subject = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class ProCourses(models.Model):
-    "Tutors ProCourses Model"
-    ProCourseName = models.CharField(max_length=100)
-    nunmberOfTutors = models.IntegerField(null=True)
-    relatedProCourses = models.TextField(max, null=True)
-    created_at= models.DateField(auto_now_add=True)
-    updated_at = models.DateField(default=timezone.now(), null=True)
+class ProCourse(models.Model):
+    """Tutors ProCourses Model"""
+    pro_course_name = models.CharField(max_length=100)
+    number_of_tutors = models.IntegerField(null=True)
+    related_pro_courses = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now, null=True)
