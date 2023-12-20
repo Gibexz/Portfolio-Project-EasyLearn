e90cb8047401f1d87e7d1d453e9991203be52e21
chimekinglsey <onlinekingsley@gmail.com>
2023-12-21 00:32:33 +0100

    updates on landing page

---
+++ Dynamic/LessonPediaProject/static/script/landing_page.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".other_nav").hover(function(){
+        $(this).css("color", "green")
+        $("#default").css("color", "black")
+    })
+
+    $(".other_nav").mouseleave(function(){
+        $(this).css("color", "black")
+        $("#default").css("color", "green")
+    })
+
+    $(".rightArrow").click(function(){
+        $(this).closest(".content").find("p").toggle();
+    });
+})
+
+// dropdown menu tweak
+$(document).ready(function(){
+    $(".login_btn").click(function(){
+        $(".dropdown-content").toggle();
+    });
+
+    // Close dropdown when clicking outside
+    $(document).on("click", function(event){
+        if(!$(event.target).closest(".dropdown").length){
+            $(".dropdown-content").hide();
+        }
+    });
+});
