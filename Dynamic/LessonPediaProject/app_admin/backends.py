512d7fd345b6fede78689816ccc7f8fdff1da8b7
Gibexz <andrewetedu@gmail.com>
2023-12-26 22:56:43 +0100

    Andrew-AppAdmin-3 branch

--- Dynamic/LessonPediaProject/app_admin/backends.py
+++ Dynamic/LessonPediaProject/app_admin/backends.py
@@ -5,17 +5,21 @@ from .models import AppAdmin
 class AppAdminAuthBackend(ModelBackend):
     """Validation of appadmin login"""
     def authenticate(self, request, username=None, password=None, **kwargs):
-        """"""
         try:
             user = AppAdmin.objects.get(email=username)
             if user.check_password(password):
                 return user
-            else:
-                user = AppAdmin.objects.get(username=username)
-                if user.check_password(password):
-                    return user    
         except AppAdmin.DoesNotExist:
-            None
+            pass
+
+        try:
+            user = AppAdmin.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except AppAdmin.DoesNotExist:
+            pass
+
+        return None
 
     def get_user(self, user_id):
         try:
