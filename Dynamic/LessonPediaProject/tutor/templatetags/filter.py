4536906ecca2f245f9f6a4a69e08e990a5ce0b3e
chimekinglsey <onlinekingsley@gmail.com>
2024-01-06 04:47:17 +0100

    Fixed broken html that deserves to be called 'wicked'

--- Dynamic/LessonPediaProject/tutor/templatetags/filter.py
+++
@@ -1,9 +0,0 @@
-# Create a templatetags directory in your app
-
-from django import template
-
-register = template.Library()
-
-@register.filter(name='increment')
-def increment(value):
-    return value + 1
